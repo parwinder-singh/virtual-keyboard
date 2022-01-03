616ec6ac05aa7cc80dc410f489129d7251dc3b68
Parwinder <singhparwinder672@gmail.com>
2022-01-03 19:28:42 +0530

    Refactored app module

--- projects/virtual-keyboard-demo/src/app/app.module.ts
+++ projects/virtual-keyboard-demo/src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { KeyboardDirective } from './keyboard/directives/keyboard.directive';
 import {KeyboardModule} from "./keyboard/keyboard.module";
 import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
-    KeyboardDirective
   ],
   imports: [
     BrowserModule,
